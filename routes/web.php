74dbe2d20f981f59838ecb6c8d2d08bb42f944b0
Adi Putra <m.a.syaputra.94@gmail.com>
2023-08-07 17:15:20 +0700

    fix (route) middleware auth

--- routes/web.php
+++ routes/web.php
@@ -23,20 +23,29 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::prefix('roles')->group(function () {
+        Route::get('/', [RoleController::class, 'index'])->name('roles.index');
+        Route::post('/', [RoleController::class, 'store'])->name('roles.store');
+        Route::put('/{role}', [RoleController::class, 'update'])->name('roles.update');
+        Route::delete('/{role}', [RoleController::class, 'destroy'])->name('roles.destroy');
+    });
+    Route::prefix('permissions')->group(function () {
+        Route::get('/', [PermissionController::class, 'index'])->name('permissions.index');
+        Route::post('/', [PermissionController::class, 'store'])->name('permissions.store');
+        Route::put('/{permission}', [PermissionController::class, 'update'])->name('permissions.update');
+        Route::delete('/{permission}', [PermissionController::class, 'destroy'])->name('permissions.destroy');
+    });
+    Route::prefix('users')->group(function () {
+        Route::get('/', [UserController::class, 'index'])->name('users.index');
+        Route::post('/', [UserController::class, 'store'])->name('users.store');
+        Route::get('/{id}', [UserController::class, 'show'])->name('users.show');
+        Route::put('/{id}', [UserController::class, 'update'])->name('users.update');
+        Route::delete('/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+    });
 
-Route::prefix('roles')->group(function () {
-    Route::get('/', [RoleController::class, 'index'])->name('roles.index');
-    Route::post('/', [RoleController::class, 'store'])->name('roles.store');
-    Route::put('/{role}', [RoleController::class, 'update'])->name('roles.update');
-    Route::delete('/{role}', [RoleController::class, 'destroy'])->name('roles.destroy');
 });
-Route::prefix('permissions')->group(function () {
-    Route::get('/', [PermissionController::class, 'index'])->name('permissions.index');
-    Route::post('/', [PermissionController::class, 'store'])->name('permissions.store');
-    Route::put('/{permission}', [PermissionController::class, 'update'])->name('permissions.update');
-    Route::delete('/{permission}', [PermissionController::class, 'destroy'])->name('permissions.destroy');
-});
 
-Route::resource('users', UserController::class);
+
